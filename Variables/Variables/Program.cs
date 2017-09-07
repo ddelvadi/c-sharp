ad5c8443ec2eeffd26ad58bf27b42288b95fa558
ddelvadi <ddelvadi@hawk.iit.edu>
2017-09-06 23:46:08 -0500

    Commented out old code and added code that takes input from users and prints it in the console

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,12 +6,25 @@ namespace Variables
     {
         static void Main(string[] args) 
         {
+            /*
             int x, y;
             x = 7;
             y = x + 3;
 
             Console.WriteLine(y);
             Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name: ");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName + " ");
         }
     }
 }
